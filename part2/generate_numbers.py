010d5c9d32659447a0b39c51e01014b84cfbb25f
Betsy <betrakiss@hotmail.com>
2014-10-11 19:05:24 +0300

    Fixed generate_numbers.py

--- part2/generate_numbers.py
+++ part2/generate_numbers.py
@@ -3,26 +3,25 @@ import sys
 from random import randint
 
 
-def main():
-    path = sys.argv[1]
-    count = sys.argv[2]
-    print(path)
-    print(count)
-
-    generate_numbers(count, path)
-
-
 def generate_numbers(n, path):
     wfile = open(path, 'w')
     content = ""
 
+    print(n)
     for i in range(n):
-        print(i)
-        content += str(randint(1, 1000))
-        content += ' '
+        content += str(randint(1, 1000)) + ' '
+
+    # content = "Malko tupo sladko kote"
 
     wfile.write(content)
     wfile.close()
+
+
+def main():
+    path = sys.argv[1]
+    count = int(sys.argv[2])
+
+    generate_numbers(count, path)
 
 if __name__ == '__main__':
     main()
