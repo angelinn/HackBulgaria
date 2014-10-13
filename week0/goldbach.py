03aa8ec1feeaf5f8bd90fefa173b6c7943f448a6
Betsy <betrakiss@hotmail.com>
2014-10-13 15:24:40 +0300

    Adding goldbach.py, not working good.

--- week0/goldbach.py
+++ week0/goldbach.py
@@ -1,4 +1,39 @@
+from is_prime import is_prime
+
+
 def goldbach(n):
+    result = []
     limit = n
-    for i in range(limit):
-        
+
+    for i in range(2, limit):
+        print("I IS " + str(i))
+        for j in range(i + 1, n):
+            print(j)
+            if is_prime(j) is True:
+                print("IN")
+                first = j
+                n -= first
+                print(first)
+                print(n)
+                print("___")
+                print(n)
+                for j in range(n, 0, -1):
+                    print("^^^")
+                    print(j)
+                    print(n)
+                    if is_prime(j) and n - j == 0:
+                        print("IN SECOND")
+                        result.append((first, j))
+                        break
+
+    if len(result) == 0:
+        return False
+
+    return result
+
+
+def main():
+    print(goldbach(100))
+
+if __name__ == '__main__':
+    main()
