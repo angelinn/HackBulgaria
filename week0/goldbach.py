a6d33eee0e043695bc4c869b876ac983c9715168
Betsy <betrakiss@hotmail.com>
2014-10-13 15:37:51 +0300

    Fixing some stuff on goldbach.py

--- week0/goldbach.py
+++ week0/goldbach.py
@@ -6,21 +6,20 @@ def goldbach(n):
     limit = n
 
     for i in range(2, limit):
-        print("I IS " + str(i))
         for j in range(i + 1, n):
-            print(j)
             if is_prime(j) is True:
-                print("IN FIRST IF")
                 first = j
                 n -= first
-                print(first)
-                print(n)
+                print("N is " + str(n))
+                print("First is " + str(first))
+
                 for j in range(n, 0, -1):
-                    print(str(j) + " is")
                     if is_prime(j) and n - j == 0:
-                        print("IN SECOND IF")
                         result.append((first, j))
                         break
+                break
+
+        n = limit
 
     if len(result) == 0:
         return False
