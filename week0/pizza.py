50451d3f9be954b25270c60a4599571e39a7ac23
Betsy <betrakiss@hotmail.com>
2014-10-15 13:42:56 +0300

    Adding save feature to pizza.py

--- week0/pizza.py
+++ week0/pizza.py
@@ -1,3 +1,7 @@
+from time import time
+from datetime import datetime
+
+
 def take(details, orders):
     name = details[1]
     price = details[2]
@@ -10,6 +14,22 @@ def take(details, orders):
     print("Taking order from " + name + " for " + str(price))
 
 
+def status(orders):
+    for each in orders:
+        print(each + ': ' + str(orders[each]))
+
+
+def save(orders):
+    ts = time()
+    stamp = datetime.fromtimestamp(ts).strftime('%Y_%m_%d_%H_%M_%S')
+    save_file = open("orders_" + stamp, 'w')
+    for each in orders:
+        save_file.write(each + ' ' + str(orders[each]) + '\n')
+
+    save_file.close()
+    print("Saved.")
+
+
 def order():
     command = ''
     orders = {}
@@ -18,6 +38,10 @@ def order():
         command = input("Enter a command .. ")
         if command.find('take') != -1:
             take(command.split(' '), orders)
+        if command == 'status':
+            status(orders)
+        if command == 'save':
+            save(orders)
 
     print(orders)
 
