d418e0ccc3ad86477a0f53855eb60bd2e4c33837
Betsy <betrakiss@hotmail.com>
2014-10-17 20:15:58 +0300

    Adding prepare_meal_tests and fixing prepare_meal

--- week0/prepare_meal.py
+++ week0/prepare_meal.py
@@ -1,19 +1,26 @@
 def prepare_meal(n):
     result = ""
+    more_than_once = False
 
     while n > 0:
         if n % 3 == 0:
-            result = "spam " + result
+            if more_than_once is True:
+                result = "spam " + result
+            else:
+                result = "spam" + result
             n //= 3
+
+            more_than_once = True
+
         elif n % 5 == 0:
             if result == "":
-                result = "eggs "
+                result = "eggs"
             else:
-                result = result + "and eggs "
+                result = result + " and eggs"
             n //= 5
         else:
             break
 
     return result
 
-print(prepare_meal(45))
+print(prepare_meal(25))
