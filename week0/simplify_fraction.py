906785780f53ed204d107f97cda7dae5c73c6cde
Betsy <betrakiss@hotmail.com>
2014-10-20 00:38:58 +0300

    Adding test for simplify_fractions

--- week0/simplify_fraction.py
+++ week0/simplify_fraction.py
@@ -2,11 +2,12 @@ def simplify_fraction(fraction):
     first = fraction[0]
     second = fraction[1]
 
+    if second == 0:
+        raise ZeroDivisionError
+
     for i in range(max(first, second), 1, -1):
         while first % i == 0 and second % i == 0:
             first //= i
             second //= i
 
     return (first, second)
-
-print(simplify_fraction((64, 462)))
