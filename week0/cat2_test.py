f8b702861613c3f5e63e7552b3a84531cd916f52
Betsy <betrakiss@hotmail.com>
2014-10-20 16:05:38 +0300

    Adding tests for cat2.py

--- week0/cat2_test.py
+++ week0/cat2_test.py
@@ -1,4 +1,34 @@
 import unittest
 import os
-from cat2 import cat2
+from cat2 import read_multiple_content
 
+
+class Cat2Test(unittest.TestCase):
+    def setUp(self):
+        path_one = 'cat1.txt'
+        self.first_file = open(path_one, 'w')
+        self.first_content = 'mega pro voltron uber'
+        self.first_file.write(self.first_content)
+        self.first_file.flush()
+        self.first_file = open(path_one, 'r')
+
+        path_two = 'cat2.txt'
+        self.second_file = open(path_two, 'w')
+        self.second_content = 'another mega cool line'
+        self.second_file.write(self.second_content)
+        self.second_file.flush()
+        self.second_file = open(path_two, 'r')
+
+    def tearDown(self):
+        os.remove('cat1.txt')
+        os.remove('cat2.txt')
+
+    def test_one_file(self):
+        self.assertEqual(self.first_content, read_multiple_content(self.first_file))
+
+    def test_multiple_files(self):
+        self.assertEqual(self.first_content + '\n' + self.second_content,
+                         read_multiple_content(self.first_file, self.second_file))
+
+if __name__ == '__main__':
+    unittest.main()
