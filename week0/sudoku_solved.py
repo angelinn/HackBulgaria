1041b18701f79233cf1c24788fc6458bf698e705
Betsy <betrakiss@hotmail.com>
2014-10-21 20:56:23 +0300

    Fixing sudoku check for row and column, still not for 3x3 yet.

--- week0/sudoku_solved.py
+++ week0/sudoku_solved.py
@@ -1,57 +1,51 @@
-import copy
+from copy import deepcopy
+
+target = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
 
 
-def check_column(sudoku, column_number, target):
-    target_copy = copy.deepcopy(target)
-
-    for j in range(len(sudoku)):
-        for number in target_copy:
-            print(str(sudoku[column_number][j]) + ' vs ' + str(number))
-            if sudoku[column_number][j] == number:
-                print(target_copy.remove(number))
-
-    print(target_copy)
-    return target_copy == []
+def check_number(number, target):
+    if number in target:
+        target[number] += 1
+    else:
+        target[number] = 1
 
 
-def check_line(line, target):
-    target_copy = copy.deepcopy(target)
+def is_target_valid(target):
+    counter = 0
 
-    for each in line:
-        for number in target_copy:
-            if each == number:
-                target_copy.remove(number)
+    for number in target:
+        counter += 1
+        if target[number] != 1:
+            return False
 
-    print(target_copy)
-    return target_copy == []
+    return counter == 9
+
+
+def check_column(sudoku, column_number):
+    target_copy = deepcopy(target)
+
+    for j in range(9):
+        check_number(sudoku[column_number][j], target_copy)
+
+    return is_target_valid(target_copy)
+
+
+def check_line(line):
+    target_copy = deepcopy(target)
+
+    for number in line:
+        check_number(number, target_copy)
+
+    return is_target_valid(target_copy)
 
 
 def sudoku_solved(sudoku):
-    target = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
     for i, line in enumerate(sudoku):
-        if check_line(line, target) is False or check_column(sudoku, i, target) is False:
-            print("This line didn't pass the test: " + str(line))
+        if check_line(line) is False or check_column(sudoku, i) is False:
+            #print("This line didn't pass the test: " + str(line))
             return False
-        else:
-            print("This line has passed test : " + str(line))
+        # else:
+        #     print("This line has passed test : " + str(line))
 
+    return True
     print("Motherfucking Sudoku !!")
-
-
-def main():
-    sudoku_solved([
-[1, 2, 3, 4, 5, 6, 7, 8, 9],
-[1, 2, 3, 4, 5, 6, 7, 8, 9],
-[1, 2, 3, 4, 5, 6, 7, 8, 9],
-[1, 2, 3, 4, 5, 6, 7, 8, 9],
-[1, 2, 3, 4, 5, 6, 7, 8, 9],
-[1, 2, 3, 4, 5, 6, 7, 8, 9],
-[1, 2, 3, 4, 5, 6, 7, 8, 9],
-[1, 2, 3, 4, 5, 6, 7, 8, 9],
-[1, 2, 3, 4, 5, 6, 7, 8, 9]
-])
-
-
-if __name__ == '__main__':
-    main()
---
+++ week0/sudoku_solved_test.py
@@ -0,0 +1,60 @@
+import unittest
+from sudoku_solved import *
+
+
+class TestSudokuSolved(unittest.TestCase):
+    def setUp(self):
+        self.sudoku_correct = [
+                              [4, 5, 2, 3, 8, 9, 7, 1, 6],
+                              [3, 8, 7, 4, 6, 1, 2, 9, 5],
+                              [6, 1, 9, 2, 5, 7, 3, 4, 8],
+                              [9, 3, 5, 1, 2, 6, 8, 7, 4],
+                              [7, 6, 4, 9, 3, 8, 5, 2, 1],
+                              [1, 2, 8, 5, 7, 4, 6, 3, 9],
+                              [5, 7, 1, 8, 9, 2, 4, 6, 3],
+                              [8, 9, 6, 7, 4, 3, 1, 5, 2],
+                              [2, 4, 3, 6, 1, 5, 9, 8, 7]]
+
+        self.sudoku_incorrect = [
+                                [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                                [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                                [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                                [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                                [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                                [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                                [1, 2, 3, 4, 6, 6, 7, 8, 9],
+                                [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                                [1, 2, 3, 4, 5, 6, 7, 8, 9]]
+
+    def test_check_line_true(self):
+        for i in range(9):
+            self.assertTrue(check_line(self.sudoku_correct[i]))
+
+    def test_check_line_false(self):
+        for i in range(9):
+            if check_line(self.sudoku_incorrect[i]) is False:
+                self.assertFalse(check_line(self.sudoku_incorrect[i]))
+                return
+
+        self.assertFalse(True)
+
+    def test_check_col_true(self):
+        for i in range(9):
+            self.assertTrue(check_column(self.sudoku_correct, i))
+
+    def test_check_col_false(self):
+        for i in range(9):
+            if check_column(self.sudoku_incorrect, i) is False:
+                self.assertFalse(check_column(self.sudoku_incorrect, i))
+                return
+
+        self.assertFalse(True)
+
+    def test_true(self):
+        self.assertTrue(sudoku_solved(self.sudoku_correct))
+
+    def test_false(self):
+        self.assertFalse(sudoku_solved(self.sudoku_incorrect))
+
+if __name__ == '__main__':
+    unittest.main()
