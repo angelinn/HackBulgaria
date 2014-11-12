86fc647a99f4614bf4225fd640aec810966f94b0
betrakiss <betrakiss@hotmail.com>
2014-11-12 21:11:36 +0200

    Adding someth .. idk what :D

--- week5/Cinema Reservation System/reservation_system.py
+++ week5/Cinema Reservation System/reservation_system.py
@@ -1,10 +1,24 @@
 import sqlite3
+from copy import deepcopy
 
 
 class ReservationSystem:
+    MAX_SEATS = 100
+
     def __init__(self):
         self.database = None
         self.cursor = None
+
+        self.cinema_hall = [['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.']]
 
         self.create_databases()
 
@@ -12,7 +26,8 @@ class ReservationSystem:
         self.database = sqlite3.connect('movies.db')
         self.cursor = self.database.cursor()
 
-        self.cursor.executemany('PRAGMA foreign_keys=ON')
+        self.cursor.execute('PRAGMA foreign_keys=ON')
+
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS
             Movies(id INTEGER PRIMARY KEY, name TEXT, rating REAL)''')
 
@@ -29,8 +44,35 @@ class ReservationSystem:
         self.add_movie('Wreck-It Ralph', 7.8)
         self.add_movie('Her', 8.3)
 
+    def fill_projections(self):
+        self.add_projection(1, '3D', '2014-04-01', '19:10')
+        self.add_projection(1, '2D', '2014-04-01', '19:00')
+        self.add_projection(1, '4DX', '2014-04-02', '21:00')
+        self.add_projection(3, '2D', '2014-04-05', '20:20')
+        self.add_projection(2, '3D', '2014-04-02', '22:00')
+        self.add_projection(2, '2D', '2014-04-02', '19:30')
+
+    def fill_reservations(self):
+        self.add_reservation('RadoRado', 1, 2, 1)
+        self.add_reservation('RadoRado', 1, 3, 5)
+        self.add_reservation('RadoRado', 1, 7, 8)
+        self.add_reservation('Ivo', 3, 1, 1)
+        self.add_reservation('Ivo', 3, 1, 2)
+        self.add_reservation('Mysterious', 5, 2, 3)
+        self.add_reservation('Mysterious', 5, 2, 4)
+
     def add_movie(self, name, rating):
         self.cursor.execute('INSERT INTO Movies(name, rating) VALUES(?, ?)', (name, rating))
+        self.database.commit()
+
+    def add_projection(self, movie_id, type, date, time):
+        self.cursor.execute('''INSERT INTO Projections(movie_id, type, date, time)
+            VALUES(?, ?, ?, ?)''', (movie_id, type, date, time))
+        self.database.commit()
+
+    def add_reservation(self, username, projection_id, row, col):
+        self.cursor.execute('''INSERT INTO Reservations(username, projection_id, row, col)
+            VALUES(?, ?, ?, ?)''', (username, projection_id, row, col))
         self.database.commit()
 
     def remove_movie(self, name):
@@ -39,7 +81,28 @@ class ReservationSystem:
 
     def show_movies(self):
         movies = self.cursor.execute('SELECT * FROM Movies ORDER BY rating DESC')
+
         for movie in movies:
             print('[{}] - {} ({})'.format(movie[0], movie[1], movie[2]))
 
-ReservationSystem().show_movies()
+    def make_reservation(self):
+        name = input('Input name > ')
+        tickets = input('Number of tickets > ')
+
+        print('Current Movies: ')
+        self.show_movies()
+
+        choice = input('Choose a movie > ')
+        movie_name = self.cursor.execute('SELECT name FROM Movies WHERE ? = id', (choice,)).fetchone()[0]
+
+        print('Projections for {}: '.format(movie_name))
+        projections = self.cursor.execute('SELECT id, date, time, type FROM Projections WHERE movie_id = ?',
+                                          (choice,))
+        for proj in projections:
+            #slots = self.cursor.execute('SELECT Projections. )
+            print('[{}] - {} {} ({})'.format(proj[0], proj[1], proj[2], proj[3]))
+
+    #def go(self):
+
+
+ReservationSystem().make_reservation()
