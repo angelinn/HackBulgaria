82ee83cdafc543aaacb49593ec4b0348f497f0b4
betrakiss <betrakiss@hotmail.com>
2014-11-17 22:49:36 +0200

    Deleting doubled files.

--- week5/sql_test.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-connection = sqlite3.connect('polyglot.db')
-cursor = connection.cursor()
-
-result = cursor.execute("SELECT id, language FROM languages")
-
-for row in result:
-    print(row)
