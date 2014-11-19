ae35bb17ff0e02b48d9774e50761b40e4b454f33
betrakiss <betrakiss@hotmail.com>
2014-11-19 15:58:37 +0200

    Password security is now ok.

--- week5/money-in-the-bank-master/tests/sql_manager_test.py
+++ week5/money-in-the-bank-master/tests/sql_manager_test.py
@@ -10,21 +10,24 @@ class SqlManagerTests(unittest.TestCase):
 
     def setUp(self):
         sql_manager.create_clients_table()
-        sql_manager.register('Tester', '123sdiaudoiajsida')
+        sql_manager.register('Tester', '123sdiaudoI@jsida')
 
     def tearDown(self):
         sql_manager.cursor.execute('DROP TABLE clients')
 
     def test_register(self):
-        sql_manager.register('Dinko', '123121233')
+        sql_manager.register('Dinko', '123121233Aa@')
 
-        sql_manager.cursor.execute('SELECT Count(*)  FROM clients WHERE username = ? AND password = ?', ('Dinko', '123121233'))
+        sql_manager.cursor.execute('''SELECT Count(*)
+                                      FROM clients
+                                      WHERE username = ? AND password = ?''', ('Dinko', '123121233Aa@'))
+
         users_count = sql_manager.cursor.fetchone()
 
         self.assertEqual(users_count[0], 1)
 
     def test_login(self):
-        logged_user = sql_manager.login('Tester', '123sdiaudoiajsida')
+        logged_user = sql_manager.login('Tester', '123sdiaudoI@jsida')
         self.assertEqual(logged_user.get_username(), 'Tester')
 
     def test_login_wrong_password(self):
@@ -32,14 +35,14 @@ class SqlManagerTests(unittest.TestCase):
         self.assertFalse(logged_user)
 
     def test_change_message(self):
-        logged_user = sql_manager.login('Tester', '123sdiaudoiajsida')
+        logged_user = sql_manager.login('Tester', '123sdiaudoI@jsida')
         new_message = "podaivinototam"
         sql_manager.change_message(new_message, logged_user)
         self.assertEqual(logged_user.get_message(), new_message)
 
     def test_change_password(self):
-        logged_user = sql_manager.login('Tester', '123sdiaudoiajsida')
-        new_password = "12345"
+        logged_user = sql_manager.login('Tester', '123sdiaudoI@jsida')
+        new_password = "123sdiaudoI@jsida"
         sql_manager.change_pass(new_password, logged_user)
 
         logged_user_new_password = sql_manager.login('Tester', new_password)
@@ -52,11 +55,11 @@ class SqlManagerTests(unittest.TestCase):
     def test_too_short_password(self):
         self.assertFalse(sql_manager.register('Penko', '123'))
 
-    def test_lowercase_password(self):
-
-    def test_uppercase_password(self):
+    def test_password_without_number(self):
+        self.assertFalse(sql_manager.register('Penko', 'abcdefghjiK@'))
 
     def test_without_special_symbol_password(self):
+        self.assertFalse(sql_manager.register('Penko', 'abceffefes1A'))
 
 if __name__ == '__main__':
     unittest.main()
