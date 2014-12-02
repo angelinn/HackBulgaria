23cf88299480dd0f2c4ca4de44111f1e27ddf7af
Betrakiss <betrakiss@hotmail.com>
2014-12-02 21:14:10 +0200

    You can now answer questions and correctly!

--- DoYouEvenMath/database_manager.py
+++ DoYouEvenMath/database_manager.py
@@ -4,13 +4,14 @@ from connection import Base, engine
 from sqlalchemy.orm import Session
 from random_operation import RandomOperation
 from random import randint
+from sqlalchemy.exc import SQLAlchemyError
 
 
 class DatabaseManager:
     QUESTION_PREFIX = 'What is the answer of {} {} {}'
 
     def __init__(self):
-        Base.metadata.drop_all(engine)
+        #Base.metadata.drop_all(engine)
         Base.metadata.create_all(engine)
         self.session = Session(bind=engine)
 
@@ -31,9 +32,16 @@ class DatabaseManager:
         self.session.commit()
 
     def add_user(self, name):
-        self.session.add(User(name=name, score=-1))
-        self.session.commit()
+        if self.session.query(User).filter(User.name == name) is None:
+            self.session.add(User(name=name, score=-1))
+            self.session.commit()
 
     def set_score(self, user, score):
-        self.session.query(User).filter(name=user.name).update(score=score)
+        self.session.query(User).filter(name=User.name).update(score=score)
         self.session.commit()
+
+    def get_question_count(self):
+        return self.session.query(Question).count()
+
+    def get_question(self, number):
+        return self.session.query(Question).filter(Question.id == number).one()
--- DoYouEvenMath/game.py
+++ DoYouEvenMath/game.py
@@ -4,12 +4,43 @@ from database_manager import DatabaseManager
 
 
 class Game:
+    DEFAULT_QUESTION_SESSION = 10
+
     def __init__(self):
-        manager = DatabaseManager()
-        manager.add_user(input('Enter your username: '))
+        self.manager = DatabaseManager()
+        self.user = input('Enter Username: ')
 
-        for i in range(10):
-            manager.add_random_question()
+        self.manager.add_user(self.user)
+        self.current_question = 1
+
+    def start(self):
+        questions_length = self.manager.get_question_count()
+        ques = None
+
+        while questions_length > 0:
+            ques = self.manager.get_question(self.current_question)
+            self.current_question += 1
+
+            print(ques.question)
+
+            answer = input('Get answer: ')
+
+            if ques.answer == int(answer):
+                print('Correct!\n')
+            else:
+                print('Wrong!\n')
+                break
+
+            if questions_length == 1:
+                for i in range(self.DEFAULT_QUESTION_SESSION):
+                    print('Adding more questions!')
+                    self.manager.add_random_question()
+
+                questions_length = self.manager.get_question_count()
+                print(questions_length)
+
+            questions_length -= 1
 
 
-Game()
+
+Game().start()
