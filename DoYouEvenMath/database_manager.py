2d55c6a5fa8315af4dee2e804d8c961c78879a39
Betrakiss <betrakiss@hotmail.com>
2014-12-10 16:03:24 +0200

    Final version commiting.

--- DoYouEvenMath/database_manager.py
+++ DoYouEvenMath/database_manager.py
@@ -4,16 +4,18 @@ from connection import Base, engine
 from sqlalchemy.orm import Session
 from random_operation import RandomOperation
 from random import randint
-from sqlalchemy.exc import SQLAlchemyError
 
 
 class DatabaseManager:
     QUESTION_PREFIX = 'What is the answer of {} {} {}'
 
     def __init__(self):
-        #Base.metadata.drop_all(engine)
         Base.metadata.create_all(engine)
         self.session = Session(bind=engine)
+
+    def __del__(self):
+        self.session.query(Question).delete()
+        self.session.commit()
 
     def add_question_console(self):
         ques = input('Enter the question: ')
@@ -26,22 +28,28 @@ class DatabaseManager:
         oper = RandomOperation(randint(0, 20), randint(0, 20))
         oper.do_real_operation()
 
-        print('{} {} {}'.format(oper.a, oper.sign, oper.b))
         self.session.add(Question(question=self.QUESTION_PREFIX.format(oper.a, oper.sign, oper.b),
-                         answer=oper.result))
+                                  answer=oper.result))
         self.session.commit()
 
     def add_user(self, name):
-        if self.session.query(User).filter(User.name == name) is None:
+        try:
+            self.session.query(User).filter(User.name == name).one()
+
+        except Exception:
             self.session.add(User(name=name, score=-1))
             self.session.commit()
 
     def set_score(self, user, score):
-        self.session.query(User).filter(User.name == user).update({'score': score})
-        self.session.commit()
+        if score > self.session.query(User).filter(User.name == user).one().score:
+            self.session.query(User).filter(User.name == user).update({'score': score})
+            self.session.commit()
 
     def get_question_count(self):
         return self.session.query(Question).count()
 
     def get_question(self, number):
         return self.session.query(Question).filter(Question.id == number).one()
+
+    def get_highscores(self):
+        return self.session.query(User).all()
--- DoYouEvenMath/game.py
+++ DoYouEvenMath/game.py
@@ -8,17 +8,35 @@ class Game:
 
     def __init__(self):
         self.manager = DatabaseManager()
-        self.user = input('Enter Username: ')
+        self.user = None
 
-        self.manager.add_user(self.user)
         self.current_question = 1
+        self.question_count = self.manager.get_question_count()
 
-    def start(self):
-        questions_length = self.manager.get_question_count()
+    def show_highscores(self):
+        users = self.manager.get_highscores()
+        for user in users:
+            print('{} - {}'.format(user.name, user.score))
+
+    def add_more_questions(self):
+        print('Loading ..\n')
+        for i in range(self.DEFAULT_QUESTION_SESSION):
+            self.manager.add_random_question()
+
+        self.question_count = self.manager.get_question_count()
+
+    def prompt_user(self):
+        self.user = input('Enter Username: ')
+        self.manager.add_user(self.user)
+
+    def ask(self):
+        self.prompt_user()
+        self.add_more_questions()
+
         ques = None
         score = 0
 
-        while questions_length > 0:
+        while self.question_count > 0:
             ques = self.manager.get_question(self.current_question)
             self.current_question += 1
 
@@ -26,25 +44,38 @@ class Game:
 
             answer = input('Get answer: ')
 
-            if ques.answer == int(answer):
-                print('Correct!\n')
-                score += 1
-            else:
-                print('Wrong!\n')
-                break
+            try:
+                if ques.answer == int(answer):
+                    print('Correct!\n')
+                    score += 2
+                else:
+                    print('Wrong!\n')
+                    break
+            except ValueError:
+                print('Please enter a valid answer.')
+                self.current_question -= 1
+                continue
 
-            if questions_length == 1:
-                for i in range(self.DEFAULT_QUESTION_SESSION):
-                    print('Adding more questions!')
-                    self.manager.add_random_question()
+            if self.question_count == 1:
+                self.add_more_questions()
 
-                questions_length = self.manager.get_question_count()
-                print(questions_length)
-
-            questions_length -= 1
+            self.question_count -= 1
 
         self.manager.set_score(self.user, score)
 
+    def start(self):
+        while True:
+            command = input("> ")
+            if command == 'start':
+                self.ask()
+            elif command == 'highscores':
+                self.show_highscores()
+            elif command == 'exit':
+                return
 
 
-Game().start()
+def main():
+    Game().start()
+
+if __name__ == '__main__':
+    main()
